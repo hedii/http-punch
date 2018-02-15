fef1d1d07be19be062fba736bd341c2734631b71
hedii <contact@hedichaibi.com>
2018-02-15 15:05:56 +0100

    Fix test server

--- tests/server/public/index.php
+++ tests/server/public/index.php
@@ -6,32 +6,32 @@ $app = new Laravel\Lumen\Application(
     realpath(__DIR__ . '/../')
 );
 
-$app->get('/get', function () {
+$app->router->get('/get', function () {
     return response()->json(['message' => 'response ok']);
 });
 
-$app->post('/post', function () {
+$app->router->post('/post', function () {
     return response()->json(['message' => 'response to post ok']);
 });
 
-$app->get('/endpoint-ok', function () {
+$app->router->get('/endpoint-ok', function () {
     return response()->json(['message' => 'response ok'], 200);
 });
 
-$app->get('/endpoint-forbidden', function () {
+$app->router->get('/endpoint-forbidden', function () {
     return response()->json(['message' => 'forbidden'], 403);
 });
 
-$app->get('/endpoint-error', function () {
+$app->router->get('/endpoint-error', function () {
     return response()->json(['message' => 'internal error'], 500);
 });
 
-$app->get('/endpoint-timeout', function () {
+$app->router->get('/endpoint-timeout', function () {
     sleep(2);
     return response()->json(['message' => 'response ok to timeout']);
 });
 
-$app->get('/endpoint-redirect', function () {
+$app->router->get('/endpoint-redirect', function () {
     return redirect('/endpoint-ok');
 });
 
